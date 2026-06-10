229309f18f64286fce8c923fe52c8b28c8b03382
nlo-yoghashri <yoghashri.srimathi@needlist.org>
2026-06-10 01:00:10 -0400

    added mock files to the project for testing

---
+++ app.py
@@ -0,0 +1,7 @@
+from config import APP_NAME
+
+def main():
+    print(f"Welcome to {APP_NAME}")
+
+if __name__ == "__main__":
+    main()
---
+++ config.py
@@ -0,0 +1,2 @@
+APP_NAME = "Gitleaks Test Project"
+VERSION = "1.0.0"
