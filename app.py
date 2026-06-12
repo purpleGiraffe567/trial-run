2ab2ce481b907e942ddf9af1cdb2be011a574096
nlo-yoghashri <yoghashri.srimathi@needlist.org>
2026-06-12 13:52:50 -0400

    changed app.py for no reason

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from config import APP_NAME
 user_id = "Adihedijeoeiewoihog"
 
 def main():
-    print(f"Welcome to my {APP_NAME}. Your user ID is {user_id}.")
+    print(f"Welcome to {APP_NAME}. Your user ID is {user_id}.")
 
 if __name__ == "__main__":
     main()
