647b6ddd4ff5f14fa3e108cb798bebebddea6e1a
nlo-yoghashri <yoghashri.srimathi@needlist.org>
2026-06-12 13:49:23 -0400

    added github workflow actions

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from config import APP_NAME
+user_id = "Adihedijeoeiewoihog"
 
 def main():
-    print(f"Welcome to my {APP_NAME}")
+    print(f"Welcome to my {APP_NAME}. Your user ID is {user_id}.")
 
 if __name__ == "__main__":
     main()
